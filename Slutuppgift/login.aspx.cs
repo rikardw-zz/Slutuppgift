19a32a9817ea4a73a4a1998faea8e4cbc5ea19bf
Patrick Nyfeldt <patrick.nyfeldt@hotmail.com>
2013-11-07 16:07:44 +0100

    Inga ändringar
    Har försökt ordna med login via xml men har inte fått det att fungera.

--- Slutuppgift/login.aspx.cs
+++ Slutuppgift/login.aspx.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 using System.Web.Security; //möjliggör inloggning
+
 
 namespace Slutuppgift
 {
     public partial class WebForm4 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+       
 
+        protected void Page_Load(object sender, EventArgs e)  
+        {
+            
         }
 
         protected void submit_Click(object sender, EventArgs e)
         {
+
             if (name.Text == "Admin" && password.Text == "123"){
                 FormsAuthentication.RedirectFromLoginPage(name.Text, false);             
             }
         }
+
+        
+
     }
 }
